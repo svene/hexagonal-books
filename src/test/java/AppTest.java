cf56845daee931c46192331b58251894a8e190c8
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2020-12-06 08:08:42 +0100

    assertj support. Note that 'click to show difference' in IDEA is not shown anymore when assertJ's assertions are used

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test public void greeting_junit() {
         App classUnderTest = new App();
         assertEquals("Hello world.", classUnderTest.getGreeting());
+    }
+    @Test public void greeting_assertj() {
+        App classUnderTest = new App();
+        assertThat(classUnderTest.getGreeting()).isEqualTo("Hello world.");
     }
 }
