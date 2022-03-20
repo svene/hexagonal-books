584a0bbff1cdd0f2815f248b7cab74bcce145669
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2022-03-20 11:39:25 +0100

    spring-boot setup

--- src/test/java/org/example/hexagonalarchitecture/books/AppTest.java
+++ src/test/java/org/example/hexagonalarchitecture/books/AppTest.java
@@ -1,26 +1,5 @@
 package org.example.hexagonalarchitecture.books;
 
-import net.jqwik.api.Example;
-import org.example.hexagonalarchitecture.books.App;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class AppTest {
-    @Test public void greeting_junit() {
-        App classUnderTest = new App();
-        assertEquals("Hello world.", classUnderTest.getGreeting());
-    }
-    @Test public void greeting_assertj() {
-        App classUnderTest = new App();
-        assertThat(classUnderTest.getGreeting()).isEqualTo("Hello world.");
-    }
-
-    @Example
-    public void greeting_jqwik_example() {
-        App classUnderTest = new App();
-        assertThat(classUnderTest.getGreeting()).isEqualTo("Hello world.");
-    }
 
 }
